5df6645488fb8cefc6302f8470180cd57aa5448e
AarooneyFarr <Aarooney.Farr@gmail.com>
2016-10-03 07:57:37 -0600

    linked the added view to its associated class
    Errors everywhere, not sure why

---
+++ PracticeProject/untitled folder/updatedViewController.swift
@@ -0,0 +1,17 @@
+//
+//  updatedViewController.swift
+//  PracticeProject
+//
+//  Created by Farr, Aaron on 10/3/16.
+//  Copyright © 2016 Farr, Aaron. All rights reserved.
+//
+
+import UIKit
+
+class updatedViewController: UIViewController{
+    
+    override func viewDidload(){
+        super.viewDidLoad()
+        //load this pages components.
+    }
+}
